e64399aff5b3af18f681992655bd72077f7f94d2
Aurélien <aurelien.lamberger@epitech.eu>
2025-11-30 10:33:58 +0100

    [FEAT] Color Picker with element of the right size

--- src/components/editor/Toolbar.tsx
+++ src/components/editor/Toolbar.tsx
@@ -1,6 +1,9 @@
+import { useState } from 'react';
+import { Palette } from 'lucide-react';
 import { TOOLS } from '../../config/tools.config';
 import type { ToolType } from '../../types/index';
 import { ToolButton } from '../ToolBar/ToolButton';
+import { ColorPickerModal } from '../ToolBar/ColorPickerModal';
 
 interface ToolbarProps {
   activeTool: ToolType;
@@ -14,9 +17,15 @@ interface ToolbarProps {
 function Toolbar({
   activeTool,
   onToolChange,
+  primaryColor,
+  // secondaryColor,
+  onPrimaryColorChange,
+  // onSecondaryColorChange,
 }: ToolbarProps) {
+  const [isPickerOpen, setIsPickerOpen] = useState(false);
   return (
     <div className="bg-slate-800 border-r border-slate-700 w-20 flex flex-col items-center py-4 space-y-4">
+      {/* Outils */}
       <div className="space-y-1">
         {TOOLS.map((tool) => (
           <ToolButton
@@ -27,6 +36,21 @@ function Toolbar({
           />
         ))}
       </div>
+        <button
+          onClick={() => setIsPickerOpen(true)}
+          className="relative w-12 h-12 rounded-lg border-2 border-slate-600 hover:border-slate-400 transition-all"
+          style={{ backgroundColor: primaryColor }}
+        >
+          <Palette size={16} className="absolute bottom-0 right-0 text-white bg-slate-800 rounded-full p-0.5" />
+        </button>
+
+        {/* Modal */}
+        <ColorPickerModal
+          isOpen={isPickerOpen}
+          currentColor={primaryColor}
+          onClose={() => setIsPickerOpen(false)}
+          onColorChange={onPrimaryColorChange}
+        />
     </div>
   );
 }
